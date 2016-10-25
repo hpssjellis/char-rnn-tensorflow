7299cdd2ad43947bfbbd70a2056897d0a6ac7ba6
Jeremy Ellis <keyfreemusic@gmail.com>
2016-10-25 11:31:56 -0700

    Update rnn-serve.php

--- rnn-serve.php
+++ rnn-serve.php
@@ -13,19 +13,24 @@ echo "<br><br>Results from sample-tinyshakespeare.py...<br>";
 echo "sent:  python sample-tinyshakespeare.1.py --save_dir $phpGo1 -n $phpGo2 --prime $phpGo3 --sample $phpGo4<br><hr><br>";
 
      
-$output = passthru("python sample-tinyshakespeare.1.py --save_dir ".$phpGo1." -n ".$phpGo2." --prime ".$phpGo3." --sample ".$phpGo4);
+//$output = shell_exec("python sample-tinyshakespeare.1.py --save_dir ".$phpGo1." -n ".$phpGo2." --prime ".$phpGo3." --sample ".$phpGo4);
 //$output = passthru("python sample-tinyshakespeare.1.py");
      
-echo $output;
+
      
 
 
 // the following attempts to insert <br> line breaks don't work
 
-//$output = preg_replace('#(\r\n?|\n)#', '<br>$1', passthru("python keras-serve.py ".$phpGo1." ".$phpGo2." ".$phpGo3." ".$phpGo4));
+$output = preg_replace('#(\r\n?|\n)#', '<br>$1', shell_exec("python sample-tinyshakespeare.1.py --save_dir ".$phpGo1." -n ".$phpGo2." --prime ".$phpGo3." --sample ".$phpGo4));
 
      //nl2br(exec("python test2.py ".$phpGo1." ".$phpGo2." ".$phpGo3." ".$phpGo4));
      
 //echo preg_replace("/\r\n\r\n|\r\r|\n\n", "<br />",  $output);
+
+
+
+echo $output;
+
 ?>
 
